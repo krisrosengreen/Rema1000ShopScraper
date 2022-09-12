733ba770a3a47adb53c0e64a7324e92a20c5ee7a
kristofferivik <rosengreenpedersen@gmail.com>
2022-09-12 16:34:53 +0200

    Created a file to visualize relations to each variable scraped

---
+++ visuals.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from shops import remashop
+
+rema = remashop.Rema()
+rema.gatherItems(100, "ppkg", "d_n")
+
+items_ppkg = rema.items_all["items_ppkg"]
+
+def dictToArray(items):
+    L = []
+
+    for key in items.keys():
+        L.append(list(rema.items_all["items_ppkg"][key].values())[1:])
+
+    return np.array(L)
+
+def covarianceMap():
+    data = dictToArray(items_ppkg)
+    num_columns = len(data[0])
+
+    fig, axs = plt.subplots(num_columns, num_columns)
+
+    columns_labels = ["cp", "np", "ppc", "p_dscnt", "ippk"]
+
+    for i in range(num_columns):
+        for j in range(num_columns):
+            ax = axs[i][j]
+            
+            xs = data[:, i]
+            ys = data[:, j]
+
+            ax.scatter(xs, ys, s=1)
+
+            if i == 0:
+                ax.set_title(columns_labels[j])
+            if j == 0:
+                ax.set_ylabel(columns_labels[i])
+
+    
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    covarianceMap()
